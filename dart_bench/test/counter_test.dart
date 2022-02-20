69bb5fabdcfe8399b389cbae5680e799c969dde7
Andrei Pushkin <andpush@gmail.com>
2022-02-21 00:55:35 +0300

    Dart - fixed test

--- dart_bench/test/counter_test.dart
+++ dart_bench/test/counter_test.dart
@@ -7,9 +7,7 @@ void main() {
   test('count words', () {
     Result r = Parser(2).parse("We decided to go there. Go There, go there, go there. To be or not to be, that is the question.");
 
-    expect(r.top_words, {'go': 4, 'there': 4});
+    expect(r.topWords, {'go': 4, 'there': 4});
     expect(r.topLetters, {'e': 15, 't': 12});
-    print(r.top_words);
-    print(r.topLetters);
   });
 }
